6e41a08a40325a69c5d9baa9da90a8507f952030
Neha Neema <neha.neema@vodafone.com>
2019-04-30 17:36:16 +0530

    IntuneSDKWrapper

--- app/src/main/java/com/vodafone/intunesdkwrapper/MainActivity.java
+++ app/src/main/java/com/vodafone/intunesdkwrapper/MainActivity.java
@@ -3,11 +3,14 @@ package com.vodafone.intunesdkwrapper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.vodafone.intune_lib.authentication.AuthManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
